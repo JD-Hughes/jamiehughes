57a10fa24a1828d940cbf0245a82b6bf6fdb9efa
Jamie Hughes <jamie.hughes42@btinternet.com>
2021-10-21 15:46:05 +0100

    Create app.js

---
+++ projects/app.js
@@ -0,0 +1,9 @@
+function openArticle(){
+  var el = document.getElementById("article");
+    el.classList.add("is-active");
+}
+//關閉
+function closeArticle(){
+  var el = document.getElementById("article");
+    el.classList.remove("is-active");
+}
