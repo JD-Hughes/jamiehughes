42f060af4e6e4fed82827a226ca9a169737e7eca
Jamie <jamie.hughes42@btinternet.com>
2021-10-27 23:08:18 +0100

    added support for multiple articles

--- projects/app.js
+++ projects/app.js
@@ -1,9 +1,24 @@
-function openArticle(){
-  var el = document.getElementById("article");
+function openArticle(articleNumber){
+  var el = document.getElementById(`article-${articleNumber}`);
     el.classList.add("is-active");
 }
 //關閉
-function closeArticle(){
-  var el = document.getElementById("article");
+function closeArticle(articleNumber){
+  var el = document.getElementById(`article-${articleNumber}`);
     el.classList.remove("is-active");
+}
+
+function openLink(linkID){
+  switch (linkID) {
+    case 1:
+      window.open("https://www.chatchaos.tv/",'_blank');
+      break;
+    case 2:
+      window.open("https://www.jamiehughes.info/photography",'_blank');
+      break;
+  
+    default:
+      alert("Link not found!")
+      break;
+  }
 }
