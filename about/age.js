e906f47b0e4c6b469de94aa4a51691b34abe767f
Jamie <jamie.hughes42@btinternet.com>
2021-11-09 19:48:13 +0000

    Added many more things to the About page

--- about/age.js
+++ about/age.js
@@ -2,6 +2,7 @@ const modes = ['year','month','week','day','hour','minute','second']
 const places = [12,11,11,10,10,11,9]
 let dob = '1999-05-14'
 let ageEl = document.getElementById("age");
+let surviveEl = document.getElementById("survived");
 let modeButton = document.getElementById("mode")
 let mode = 0
 
@@ -16,3 +17,10 @@ function changeMode () {
 
     modeButton.innerText = modes[mode].toString() + 's'
 }
+
+setInterval(() => {
+	let time = dayjs().diff(dayjs(dob), 'year', true);
+    let yearsAlive = (time.toString().substring(0,12))
+    let maths = (Math.pow(yearsAlive*0.00000004521093580786,4) + Math.pow(yearsAlive*0.00000462699183828769,3) + Math.pow(yearsAlive*0.00021686105977165100,2) + yearsAlive*0.00243991806250655000 + 0.00713552195321266000)*100;
+    surviveEl.textContent = maths.toString().substring(0,10) + "%"
+}, 500);
