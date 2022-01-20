bd041ca226cbd844755df755a11ad7234331995a
Jamie <jamie.hughes42@btinternet.com>
2022-01-20 15:59:42 +0000

    Prettier 😊

--- age/age.js
+++ age/age.js
@@ -1,30 +1,30 @@
-const modes = ['year','month','week','day','hour','minute','second']
-const places = [12,11,11,10,10,11,9]
-let dob = '1999-05-14'
+const modes = ["year", "month", "week", "day", "hour", "minute", "second"];
+const places = [12, 11, 11, 10, 10, 11, 9];
+let dob = "1999-05-14";
 let ageEl = document.getElementById("age");
-let modeButton = document.getElementById("mode")
-let datePicker = document.getElementById("customDate")
-let mode = 0
+let modeButton = document.getElementById("mode");
+let datePicker = document.getElementById("customDate");
+let mode = 0;
 
 setInterval(() => {
-	let time = dayjs().diff(dayjs(dob), modes[mode], true);
-	ageEl.innerText = time.toString().substring(0, places[mode]);
+    let time = dayjs().diff(dayjs(dob), modes[mode], true);
+    ageEl.innerText = time.toString().substring(0, places[mode]);
 }, 50);
 
-function changeMode () {
-    if (mode < modes.length - 1) mode += 1
-    else mode = 0
+function changeMode() {
+    if (mode < modes.length - 1) mode += 1;
+    else mode = 0;
 
-    modeButton.innerText = modes[mode].toString() + 's'
+    modeButton.innerText = modes[mode].toString() + "s";
 }
 
 function setDate(dateInput = datePicker.value) {
-    if (dateInput == null || dateInput == '') {
+    if (dateInput == null || dateInput == "") {
         console.log("Date Picker Empty");
-        return
+        return;
     } else {
         dob = dateInput;
-        console.log("Date changed to ", dob)
+        console.log("Date changed to ", dob);
     }
 }
 //I want to be able to calculate mortality http://www.bandolier.org.uk/booth/Risk/dyingage.html
