1b24d7926733140342ac90b49d935e08ddd76be0
JD-Hughes <jamie.hughes42@btinternet.com>
2022-05-23 11:53:17 +0100

    rm click + no confetti without focus

--- schodack/birthday/app.js
+++ schodack/birthday/app.js
@@ -131,8 +131,15 @@ function hideConfetti() {
     window.cancelAnimationFrame(updateConfetti);
 }
 
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+}
+
 confettiLoop();
 function confettiLoop() {
-    addConfetti();
+    if (document.hasFocus()) {
+        addConfetti();
+    }
     setTimeout(confettiLoop, 700 + Math.random() * 1700);
 }
